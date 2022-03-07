40cc2ac115ae2fbc974492145c2120d56b07a3d8
Janette <janette.castillo91@yahoo.com>
2022-03-06 22:55:30 -0600

    added queries to front end

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client'
+
+export const QUERY_GET_ME = gql`
+query me {
+     me {
+        _id
+        username
+        email
+        bookCount 
+        savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+        }
+    }
+}`
