93516aef3640b52aa0cafc3d0d321f937d759345
Janette <janette.castillo91@yahoo.com>
2022-03-12 23:06:39 -0600

    made change to imports

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,10 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink,
+  createHttpLink
 } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { setContext } from '@apollo/client/link/context'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
