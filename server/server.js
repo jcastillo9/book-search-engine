2d32c6b5c549dbdfaac85b38b347c56bceed18bd
Janette <janette.castillo91@yahoo.com>
2022-03-13 23:30:18 -0500

    reviewing code

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 5080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -32,8 +32,8 @@ const startApolloServer = async () => {
 
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`Book Search Engine's API server running on port ${PORT}!!`)
-      console.log(`Use GraphQL to use the mutations/queries at http://localhost:${PORT}${server.graphqlPath}`)
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
 }
